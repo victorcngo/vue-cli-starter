50f092e7ce47c54642000cefe9cb3223161d915b
victorcngo <victor.cango4@gmail.com>
2021-05-19 00:18:15 +0200

    feat: add vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <hello-world msg="Vue-CLI starter based on Vue 2x + Webpack"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
